7321ba3ca38675ac52bf8398af7101669fe9a1d7
Saketh Mynampati <35532863+sMyn42@users.noreply.github.com>
2023-02-24 13:25:11 -0500

    log

---
+++ tests.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+./randmst 0 128 5 1
+# ./randmst 0 256 5 1
+# ./randmst 0 512 5 1
+# ./randmst 0 1024 5 1
+# ./randmst 0 4096 5 1
+# ./randmst 0 8192 5 1
+# ./randmst 0 16384 5 1
+
+
+./randmst 0 128 5 2
+# ./randmst 0 256 5 2
+# ./randmst 0 512 5 2
+# ./randmst 0 1024 5 2
+# ./randmst 0 4096 5 2
+# ./randmst 0 8192 5 2
+# ./randmst 0 16384 5 2
+
+
+./randmst 0 128 5 3
+# ./randmst 0 256 5 3
+# ./randmst 0 512 5 3
+# ./randmst 0 1024 5 3
+# ./randmst 0 4096 5 3
+# ./randmst 0 8192 5 3
+# ./randmst 0 16384 5 3
+
+
+./randmst 0 128 5 4
+# ./randmst 0 256 5 4
+# ./randmst 0 512 5 4
+# ./randmst 0 1024 5 4
+# ./randmst 0 4096 5 4
+# ./randmst 0 8192 5 4
+# ./randmst 0 16384 5 4
+
+# 32768; 65536; 131072;
