f2b36cf2f7dbcd905184812ff0870f74966877fd
Saketh Mynampati <35532863+sMyn42@users.noreply.github.com>
2023-02-24 13:38:00 -0500

    IMPORTANT COMMIT: finished randmst sans set optimization for iteration

--- tests.sh
+++ tests.sh
@@ -1,37 +1,51 @@
 #!/bin/sh
-./randmst 0 128 5 1
+# ./randmst 0 128 5 1
 # ./randmst 0 256 5 1
 # ./randmst 0 512 5 1
 # ./randmst 0 1024 5 1
 # ./randmst 0 4096 5 1
 # ./randmst 0 8192 5 1
 # ./randmst 0 16384 5 1
+# ./randmst 0 32768 5 1
+# ./randmst 0 65536 5 1
+# ./randmst 0 131072 5 1
+./randmst 0 262144 5 1
 
 
-./randmst 0 128 5 2
+# ./randmst 0 128 5 2
 # ./randmst 0 256 5 2
 # ./randmst 0 512 5 2
 # ./randmst 0 1024 5 2
 # ./randmst 0 4096 5 2
 # ./randmst 0 8192 5 2
 # ./randmst 0 16384 5 2
+# ./randmst 0 32768 5 2
+# ./randmst 0 65536 5 2
+# ./randmst 0 131072 5 2
+# ./randmst 0 262144 5 2
 
 
-./randmst 0 128 5 3
+# ./randmst 0 128 5 3
 # ./randmst 0 256 5 3
 # ./randmst 0 512 5 3
 # ./randmst 0 1024 5 3
 # ./randmst 0 4096 5 3
 # ./randmst 0 8192 5 3
 # ./randmst 0 16384 5 3
+# ./randmst 0 32768 5 3
+# ./randmst 0 65536 5 3
+# ./randmst 0 131072 5 3
+# ./randmst 0 262144 5 3
 
 
-./randmst 0 128 5 4
+# ./randmst 0 128 5 4
 # ./randmst 0 256 5 4
 # ./randmst 0 512 5 4
 # ./randmst 0 1024 5 4
 # ./randmst 0 4096 5 4
 # ./randmst 0 8192 5 4
 # ./randmst 0 16384 5 4
-
-# 32768; 65536; 131072;
+# ./randmst 0 32768 5 4
+# ./randmst 0 65536 5 4
+# ./randmst 0 131072 5 4
+# ./randmst 0 262144 5 4
