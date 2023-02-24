0f911c0d140598cbe520cfdf2ca717642f73f3c4
Saketh Mynampati <35532863+sMyn42@users.noreply.github.com>
2023-02-24 03:11:45 -0500

    stuff

--- randmst.cc
+++ randmst.cc
@@ -1,7 +1,3 @@
-/*
- * randmst.cc
- * Author: Saketh Mynampati 
- */
 #include <iostream>
 #include <cstdlib>
 #include <random>
@@ -29,10 +25,10 @@ using namespace std;
 
 double distance (tuple<double, double, double, double> x, tuple<double, double, double, double> y) {
     double d = 0;
-    d = d + pow(get<0>(x), 2.0) + pow(get<0>(y), 2.0);
-    d = d + pow(get<1>(x), 2.0) + pow(get<0>(y), 2.0);
-    d = d + pow(get<2>(x), 2.0) + pow(get<0>(y), 2.0);
-    d = d + pow(get<3>(x), 2.0) + pow(get<0>(y), 2.0);
+    d = d + pow(get<0>(x) - get<0>(y), 2.0);
+    d = d + pow(get<1>(x) - get<1>(y), 2.0);
+    d = d + pow(get<2>(x) - get<2>(y), 2.0);
+    d = d + pow(get<3>(x) - get<3>(y), 2.0);
     return sqrt(d);
 }
 
@@ -41,8 +37,8 @@ int main(int argc, char **argv)
 {
     // Initialize random seed.
     std::mt19937_64 gen64;
+    std::uniform_real_distribution<double> dis(0.0, 1.0);
     gen64.seed(time(0));
-    unsigned long gen_max = gen64.max();
     
     // process args
     int flag = stoi(argv[1]);
@@ -69,10 +65,12 @@ int main(int argc, char **argv)
                 for (int v = 0; v < npoints; v++) {
                     double coords[] = {0, 0, 0, 0};
                     for (int d = 0; d < ndim; d++) {
-                        coords[d] = ((float) gen64()) / gen_max;
+                        coords[d] = dis(gen64);
                     }
                     vert.push_back(make_tuple(coords[0], coords[1], coords[2], coords[3]));
                 }
+
+
 
                 break;
         }
@@ -95,6 +93,7 @@ int main(int argc, char **argv)
                 int prev[npoints];
                 int s = 0;
                 double tree_weight = 0;
+                double max_len = 0.0001;
 
                 H.insert(make_tuple(s, 0));
 
@@ -107,27 +106,36 @@ int main(int argc, char **argv)
                 while (!(H.size() == 0)) {
                     int v = get<0>(H.delmin());
                     S.insert(v);
-                    // printf("ADDED TO S");
-                    for (int u = 0; u < npoints; u++) {
+
+                    printf("Num Visited: %lu \n", S.size());
+
+                    // while (S.size() < npoints - 1) {
+                    // for (int u = 0; u < npoints; u++) {
                         for (int w = 0; w < npoints; w++) {
                             if (S.find(w) != S.end()) {
                                 continue;
-                                // printf("AVOID");
                             }
                             double ell = distance(vert[v], vert[w]);
+                            // if (ell > max_len) {
+                            //     continue;
+                            // }
+                            // Time Intensive: check if this distance is better, 
+                            // --- make this the shortest known distance and 
+                            // add it to the minheap
                             if (dist[w] > ell) {
+                                printf("AYE");
                                 dist[w] = ell;
                                 prev[w] = v;
                                 H.insert(make_tuple(w, dist[w]));
                             }
                         }
-                    }
+                    // }
+                    // }
                 }
 
                 // Calculate weights after Prim's
                 for (int v = 1; v < npoints; v++) {
                     tree_weight = tree_weight + distance(vert[v], vert[prev[v]]);
-                    printf("Integrated Vertex %i \n", v);
                 }
 
                 total_weight = total_weight + tree_weight;
